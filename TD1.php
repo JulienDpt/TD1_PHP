47ac848fdffe59223f8022161651aa050f2cb20a
d18003247 <julien.dups13116@gmail.com>
2019-09-13 11:07:02 +0200

    exo1

---
+++ TD1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TD1</title>
+    </head>
+
+    <body>
+    <?php
+        echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+        echo 'C\'est pas mal non ? <br/>';
+    ?>
+    </body>
+</html>
+
+
+
+
